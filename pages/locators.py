a72faa9ea566e7e725e79b94ec253dc0b6c485cc
SulfangRein <anton.a.chulkov@gmail.com>
2021-07-03 16:28:43 +0700

    in base_page.py add wait disappear element and wait not present element, also remove to base_page.py from main_page.py login methods in locators.py add product page locators and refactor login locators add product_page.py and methods to work this page add test_product_page.py and tests to check product page

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
@@ -8,3 +13,12 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    TOTAL_PRICE = (By.CSS_SELECTOR, '.alert-info strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    SUCCESS_PRICE = (By.CSS_SELECTOR, '.alert-success')
+    SUCCESS_ALERT = (By.CSS_SELECTOR, '.alert-success strong')
---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        add_to_basket_btn.click()
+        self.solve_quiz_and_get_code()
+        self.should_be_product_price_equals_basket_price()
+        self.should_be_product_name_equals_name_in_success_message()
+
+    def should_be_product_name_equals_name_in_success_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        success_price = self.browser.find_element(*ProductPageLocators.SUCCESS_PRICE).text
+        assert product_name == success_price, "Product name not equals in success message"
+
+    def should_be_product_price_equals_basket_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        total_price = self.browser.find_element(*ProductPageLocators.TOTAL_PRICE).text
+        assert product_price == total_price, "Product price not equals total price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_ALERT), \
+            "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_ALERT)
+
